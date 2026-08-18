dc056939047113c3818093f22c4841239ae0aabb
Aryan Tripathi <aryantripathi130306@gmail.com>
2026-08-18 15:59:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Odd or Even/odd-or-even.java
@@ -0,0 +1,21 @@
+class Solution {
+    public static boolean isEven(int n) {
+        // code here
+        int rem=n%2;
+        if(rem==0){
+            return true;
+        }   
+        else{
+            return false;
+        }
+    }
+    public static void main(String[] args){
+        int n=15;
+        if(isEven(n)==true){
+            System.out.println("true");
+        }
+        else{
+            System.out.println("false");
+        }
+    }
+}
